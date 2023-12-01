341370d6b8e6d352c13817da8758f4c9b8e98b17
matin0408 <150231931+matin0408@users.noreply.github.com>
2023-12-02 00:37:13 +0330

    Add files via upload

---
+++ matin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+main () {
+	int word;
+	cout << "please enter a word :";
+	cin>> word;
+	
+	switch(word) {
+		case 3:
+			cout<<"VOWEL";
+			  break;
+	}
+
+	
+}
